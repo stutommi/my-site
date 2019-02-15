b1197679cb91e53ac57aa03ffa9cd7ffd4a6b333
Stutommi <tommi.teetee@hotmail.com>
2019-02-15 10:11:35 +0200

    got anchor scrolling work as intended for mobile versions

--- src/js/home.js
+++ src/js/home.js
@@ -1,39 +1,22 @@
+$('body').scrollspy({ target: '#navbar-example' })
 
 
-// if (window.innerWidth > 700) {
-//   $(document).ready(function () {
-//     $("#hs1").delay(1000).slideDown(800)
-//     $("#hs2").delay(4000).slideDown(800)
-//     $("#hs3").delay(7000).slideDown(800)
-//     $("#hs4").delay(9000).slideDown(800)
-//     $("#hs5").delay(11000).fadeIn(200)
-//   });
-// } else if (window.innerHeight <= 700) {
-//   $("#hs1").removeClass('hidden');
-//   $("#hs2").removeClass('hidden');
-//   $("#hs3").removeClass('hidden');
-//   $("#hs4").removeClass('hidden');
-//   $("#hs5").removeClass('hidden');
-//   $('canvas').css({'animation': 'initial', 'opacity': '1'});
-// }
-
-
-
+// Hide collapse on click
+$('.navbar-nav>a, .nav>a').on('click', function () {
+  $('.navbar-collapse').removeClass('show')
+});
 
 (function (document) {
   var button = document.getElementById('home-button');
   var arrow = document.getElementById('home-arrow');
-  var open = false;
 
-  button.addEventListener('click', function () {
+  button.addEventListener('mouseover', function () {
+    console.log('hi');
+    arrow.className = 'home-arrow-down align-middle material-icons';
+  });
+  button.addEventListener('mouseout', function () {
     console.log('hei');
 
-    if (open) {
-      arrow.className = 'home-arrow-right align-middle material-icons';
-    } else {
-      arrow.className = 'home-arrow-down align-middle material-icons';
-    }
-
-    open = !open;
-  });
+    arrow.className = 'home-arrow-right align-middle material-icons';
+  })
 })(document);
